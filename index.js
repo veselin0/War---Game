833e372c26ec94f637cfb158bee4afa47d6cefd2
Veselin <vesselin60@gmail.com>
2021-10-19 18:03:37 +0300

    Styling part 2

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 let deckId;
-
+// create html elemnts
 // create button
 const btn = document.createElement('button');
 btn.className = 'new-deck';
 btn.innerText = 'New Deck, Please!';
 document.body.appendChild(btn);
+// create new div before the second button
+const cardsContainer = document.createElement('div');
+cardsContainer.className = 'cards-container';
+document.body.appendChild(cardsContainer);
+//create two new divs in cards container div
+const cardSlot0 = document.createElement('div');
+cardSlot0.className = 'card-slot';
+cardsContainer.appendChild(cardSlot0);
+const cardSlot1 = document.createElement('div');
+cardSlot1.className = 'card-slot';
+cardsContainer.appendChild(cardSlot1);
+// create another button
+const br = document.createElement('br');
+document.body.appendChild(br);
+const btn1 = document.createElement('button');
+btn1.className = 'draw-two-cards';
+btn1.innerText = 'Draw Two Cards, Please!';
 
 // create clickHandler function
 const clickHandler = () => {
@@ -13,21 +30,21 @@ const clickHandler = () => {
         .then(data => {
             deckId = data.deck_id;
             console.log(deckId);
-            if (deckId) drawCards();
+            if (deckId) document.body.appendChild(btn1); //drawCards();
         }); 
 };
 
 // add eventlistener to the button
 btn.addEventListener('click', clickHandler);
      
-const drawCards = () => {
-    // create another button
-    const br = document.createElement('br');
-    document.body.appendChild(br);
-    const btn1 = document.createElement('button');
-    btn1.className = 'draw-two-cards';
-    btn1.innerText = 'Draw Two Cards, Please!';
-    document.body.appendChild(btn1);
+// const drawCards = () => {
+//     // create another button
+//     const br = document.createElement('br');
+//     document.body.appendChild(br);
+//     const btn1 = document.createElement('button');
+//     btn1.className = 'draw-two-cards';
+//     btn1.innerText = 'Draw Two Cards, Please!';
+//     document.body.appendChild(btn1);
 
     // create another click-handler function
     const clickHandler1 = () => {
@@ -36,40 +53,23 @@ const drawCards = () => {
             .then(data => {
                 console.log(data); 
                 // create new div after the second button
-                const div = document.createElement('div');
-                div.className = 'img-container';
+                // const div = document.createElement('div');
+                // div.className = 'img-container';
                 //create card images
-                div.innerHTML = `
-                    <img class='img0' src=${data.cards[0].image} alt='card0' width='200' height='300'><br>
-                    <img class='img1' src=${data.cards[1].image} alt='card1' width='200' height='300'>
+                cardSlot0.innerHTML = `
+                
+                    <img class='card' src=${data.cards[0].image} alt='card0'>
                 `;
-                document.body.appendChild(div); 
+                cardSlot1.innerHTML = `
+                
+                    <img class='card' src=${data.cards[1].image} alt='card1'>
+                
+            `;
+                // document.body.appendChild(div); 
             });
     };
 
     // add anothe eventlistener to the button 1
     btn1.addEventListener('click', clickHandler1);
-};
+//};
 
-/**
- * Challenge:
- * 
- * Display the images of the 2 cards you drew in the browser.
- * Probably best to use `innerHTML` to insert a couple <img> elements
- * on the page.
-*/
-
-// const dispalyCards = () => {
-//     // create new div after the second button
-//     const div = document.createElement('div');
-//     div.className = 'img-container';
-//     //create card images
-//     div.innerHTML = `
-//         <img src='${src0}' alt='card0' width='100' height='200'><br>
-//         <img src='${src1}' alt='card1' width='100' height='200'>
-//     `;
-//     document.body.appendChild(div);
-// }
-
-//${cards[0].image}
-//${cards[1].image}
