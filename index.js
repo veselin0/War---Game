a3209c6e008d5de485d00eae461586526649632d
Veselin <vesselin60@gmail.com>
2021-10-20 10:28:53 +0300

    Determine the winning card part 1

--- index.js
+++ index.js
@@ -25,51 +25,48 @@ btn1.innerText = 'Draw Two Cards, Please!';
 
 // create clickHandler function
 const clickHandler = () => {
-    fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/') 
+    fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
         .then(response => response.json())
         .then(data => {
             deckId = data.deck_id;
-            console.log(deckId);
             if (deckId) document.body.appendChild(btn1); //drawCards();
-        }); 
+        });
 };
 
 // add eventlistener to the button
 btn.addEventListener('click', clickHandler);
-     
-// const drawCards = () => {
-//     // create another button
-//     const br = document.createElement('br');
-//     document.body.appendChild(br);
-//     const btn1 = document.createElement('button');
-//     btn1.className = 'draw-two-cards';
-//     btn1.innerText = 'Draw Two Cards, Please!';
-//     document.body.appendChild(btn1);
 
-    // create another click-handler function
-    const clickHandler1 = () => {
-        fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`) 
-            .then(response => response.json())
-            .then(data => {
-                console.log(data); 
-                // create new div after the second button
-                // const div = document.createElement('div');
-                // div.className = 'img-container';
-                //create card images
-                cardSlot0.innerHTML = `
+// create another click-handler function
+const clickHandler1 = () => {
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+        .then(response => response.json())
+        .then(data => {
+
+            cardSlot0.innerHTML = `
                 
                     <img class='card' src=${data.cards[0].image} alt='card0'>
-                `;
-                cardSlot1.innerHTML = `
+            `;
+            cardSlot1.innerHTML = `
                 
                     <img class='card' src=${data.cards[1].image} alt='card1'>
                 
             `;
-                // document.body.appendChild(div); 
-            });
-    };
+            // determine which of the 2 cards is the "winner" (has higher value)
+            const score = ["2", "3", "4", "5", "6", "7", "8", "9", 
+            "10", "JACK", "QUEEN", "KING", "ACE"];
+            let card0 = score.indexOf(data.cards[0].value);
+            let card1 = score.indexOf(data.cards[1].value);
+            if (card0 > card1) {
+                console.log('card0');
+            } else if (card1 > card0) {
+                console.log('card1');
+            } else {
+                console.log("It's a tie!");
+            }
 
-    // add anothe eventlistener to the button 1
-    btn1.addEventListener('click', clickHandler1);
-//};
 
+        });
+};
+
+// add anothe eventlistener to the button 1
+btn1.addEventListener('click', clickHandler1);
